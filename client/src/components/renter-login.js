db712f99078e1ea554ee9746828ed88d29ac7f02
Grandelisn <grandelisn@gmail.com>
2021-02-01 18:08:41 -0500

    started styling

---
+++ client/src/components/renter-login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const RenterLogin = (props) => {
+    
+  return (
+      <div className = 'renter-login'>
+          
+      </div>
+  );
+};
+export default RenterLogin;
